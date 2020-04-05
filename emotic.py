76e3905f3bd0f57c05e215abf20db0c57d847dac
Abhishek Tandon <abhishektnd@gmail.com>
2020-04-05 10:14:57 +0200

    Updated emotic.py
    Updated emotic model class to remove dependency on resnet18 as body and context model

--- emotic.py
+++ emotic.py
@@ -2,20 +2,23 @@ import torch
 import torch.nn as nn 
 
 ''' Emotic Model'''
-class Emotic18(nn.Module):
-  def __init__(self):
-    super(Emotic18,self).__init__()
-    self.fc1 = nn.Linear(1024,256)
+class Emotic(nn.Module):
+  def __init__(self, num_context_features, num_body_features):
+    super(Emotic,self).__init__()
+    self.num_context_features = num_context_features
+    self.num_body_features = num_body_features
+    self.fc1 = nn.Linear((self.num_context_features + num_body_features), 256)
     self.bn1 = nn.BatchNorm1d(256)
     self.d1 = nn.Dropout(p=0.5)
-    self.fc_cat = nn.Linear(256,26)
-    self.fc_cont = nn.Linear(256,3)
+    self.fc_cat = nn.Linear(256, 26)
+    self.fc_cont = nn.Linear(256, 3)
     self.relu = nn.ReLU()
 
+    
   def forward(self, x_context, x_body):
-    context_features = x_context.view(-1,512)
-    body_features = x_body.view(-1,512)
-    fuse_features = torch.cat((context_features,body_features),1)
+    context_features = x_context.view(-1, self.num_context_features)
+    body_features = x_body.view(-1, self.num_body_features)
+    fuse_features = torch.cat((context_features, body_features), 1)
     fuse_out = self.fc1(fuse_features)
     fuse_out = self.bn1(fuse_out)
     fuse_out = self.relu(fuse_out)
