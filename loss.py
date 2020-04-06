585df4eed6de31a47c8dd6eb627e0000d5384404
Abhishek Tandon <abhishektnd@gmail.com>
2020-04-06 13:57:49 +0200

    Updated loss.py
    Added static weights

--- loss.py
+++ loss.py
@@ -8,17 +8,21 @@ class DiscreteLoss(nn.Module):
     self.weight_type = weight_type
     if self.weight_type == 'mean':
       self.weights = torch.ones((1,26))/26.0
-    # elif self.weight_type == 'static':
+    elif self.weight_type == 'static':
+      self.weights = torch.FloatTensor([0.1435, 0.1870, 0.1692, 0.1165, 0.1949, 0.1204, 0.1728, 0.1372, 0.1620,
+         0.1540, 0.1987, 0.1057, 0.1482, 0.1192, 0.1590, 0.1929, 0.1158, 0.1907,
+         0.1345, 0.1307, 0.1665, 0.1698, 0.1797, 0.1657, 0.1520, 0.1537]).unsqueeze(0)
 
+      
   def forward(self, pred, target, device='cpu'):
     if self.weight_type == 'dynamic':
       self.weights = self.prepare_dynamic_weights(target)
-    print ('weights class', self.weights.shape, self.weights.dtype, self.weights.requires_grad)
     if device == 'gpu':
       self.weights = self.weights.cuda()
     loss = (((pred - target)**2) * self.weights)
     return loss.sum() 
 
+  
   def prepare_dynamic_weights(self, target):
     target_stats = torch.sum(target, dim=0).float().unsqueeze(dim=0).cpu()
     weights = torch.zeros((1,26))
