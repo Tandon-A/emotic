0accd2f80295603bb2a69863f3d3353ab21602a9
Abhishek Tandon <abhishektnd@gmail.com>
2020-04-08 12:26:36 +0200

    Added support for densenet161

--- prepare_models.py
+++ prepare_models.py
@@ -6,7 +6,6 @@ import os
 
 ''' Function to prepare context and body model.''' 
 def prep_models(context_model='resnet18', body_model='resnet18', model_dir='./'):
-
   model_name = '%s_places365.pth.tar' % context_model
   model_file = os.path.join(model_dir, model_name)
   if not os.path.exists(model_file):
@@ -25,20 +24,30 @@ def prep_models(context_model='resnet18', body_model='resnet18', model_dir='./')
   model = models.__dict__[context_model](num_classes=365)
 
   checkpoint = torch.load(save_file, map_location=lambda storage, loc: storage) # model trained in GPU could be deployed in CPU machine like this!
-  state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()} # the data parallel layer will add 'module' before each layer name
+  if context_model == 'densenet161':
+    state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+    state_dict = {str.replace(k,'norm.','norm'): v for k,v in state_dict.items()}
+    state_dict = {str.replace(k,'conv.','conv'): v for k,v in state_dict.items()}
+    state_dict = {str.replace(k,'normweight','norm.weight'): v for k,v in state_dict.items()}
+    state_dict = {str.replace(k,'normrunning','norm.running'): v for k,v in state_dict.items()}
+    state_dict = {str.replace(k,'normbias','norm.bias'): v for k,v in state_dict.items()}
+    state_dict = {str.replace(k,'convweight','conv.weight'): v for k,v in state_dict.items()}
+  else:
+    state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()} # the data parallel layer will add 'module' before each layer name
   model.load_state_dict(state_dict)
   model.eval()
   model.cpu()
   torch.save(model, os.path.join(model_dir, 'context_model' + '.pth'))
   
   print ('completed preparing context model')
-
+  
   model = models.__dict__[body_model](pretrained=True)
   model.cpu()
   torch.save(model, os.path.join(model_dir, 'body_model' + '.pth'))
 
   print ('completed preparing body model')
 
-prep_models(model_dir='./')
+if __name__ == '__main__':
+  prep_models(model_dir='./')
 
 
