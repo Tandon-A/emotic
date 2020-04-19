9704338447a81424f548e0d95bd36b0000e060e2
Abhishek Tandon <abhishektnd@gmail.com>
2020-04-19 20:25:43 +0200

    Added support for VAD error calculation

--- test.py
+++ test.py
@@ -18,12 +18,25 @@ def test_scikit_ap(cat_preds, cat_labels, ind2cat):
     ap[i] = average_precision_score(cat_labels[i, :], cat_preds[i, :])
     print ('Category %16s %.5f' %(ind2cat[i], ap[i]))
   print ('Mean AP %.5f' %(ap.mean()))
+  return ap 
+
+''' Calcaulate VAD (valence, arousal, dominance) errors. '''
+def test_vad(cont_preds, cont_labels):
+  vad = np.zeros(3, dtype=np.float32)
+  vad_names = ['Valence, 'Arousal', 'Dominance']
+  for i in range(3):
+    vad[i] = np.mean(np.abs(cont_preds[i, :] - cont_labels[i, :]))
+    print ('Continuous %10s %.5f' %(vad_names[i], vad[i]))
+  print ('Mean VAD %.5f' %(vad.mean()))
+  return vad
 
 ''' Test models on data '''
-def test_data(models, device, data_loader, ind2cat, num_images, save_results=False, result_dir='./'):
+def test_data(models, device, data_loader, ind2cat, num_images, result_dir='./', test_type='val'):
     model_context, model_body, emotic_model = models
     cat_preds = np.zeros((num_images, 26))
     cat_labels = np.zeros((num_images, 26))
+    cont_preds = np.zeros((num_images, 3))
+    cont_labels = np.zeros((num_images, 3))
 
     with torch.no_grad():
         model_context.to(device)
@@ -34,29 +47,35 @@ def test_data(models, device, data_loader, ind2cat, num_images, save_results=Fal
         emotic_model.eval()
         indx = 0
         print ('starting testing')
-        for images_context, images_body, labels_cat, _ in iter(data_loader):
+        for images_context, images_body, labels_cat, labels_cont in iter(data_loader):
             images_context = images_context.to(device)
             images_body = images_body.to(device)
 
             pred_context = model_context(images_context)
             pred_body = model_body(images_body)
-            pred_cat, _ = emotic_model(pred_context, pred_body)
+            pred_cat, pred_cont = emotic_model(pred_context, pred_body)
 
             cat_preds[ indx : (indx + pred_cat.shape[0]), :] = pred_cat.to("cpu").data.numpy()
             cat_labels[ indx : (indx + labels_cat.shape[0]), :] = labels_cat.to("cpu").data.numpy()
+            cont_preds[ indx : (indx + pred_cont.shape[0]), :] = pred_cont.to("cpu").data.numpy() * 10
+            cont_labels[ indx : (indx + labels_cont.shape[0]), :] = labels_cont.to("cpu").data.numpy() * 10
             indx = indx + pred_cat.shape[0]
 
     cat_preds = cat_preds.transpose()
     cat_labels = cat_labels.transpose()
+    cont_preds = cont_preds.transpose()
+    cont_labels = cont_labels.transpose()
     print ('completed testing')
     
     # Mat files used for emotic testing (matlab script)
-    if save_results == True:
-        scipy.io.savemat(os.path.join(result_dir, 'cat_preds.mat'), mdict={'cat_preds':cat_preds})
-        scipy.io.savemat(os.path.join(result_dir, 'cat_labels.mat'), mdict={'cat_labels':cat_labels})
-        print ('saved mat files')
+    scipy.io.savemat(os.path.join(result_dir, '%s_cat_preds.mat' %(test_type)), mdict={'cat_preds':cat_preds})
+    scipy.io.savemat(os.path.join(result_dir, '%s_cat_labels.mat' %(test_type)), mdict={'cat_labels':cat_labels})
+    scipy.io.savemat(os.path.join(result_dir, '%s_cont_preds.mat' %(test_type)), mdict={'cont_preds':cont_preds})
+    scipy.io.savemat(os.path.join(result_dir, '%s_cont_labels.mat' %(test_type)), mdict={'cont_labels':cont_labels})
+    print ('saved mat files')
 
     test_scikit_ap(cat_preds, cat_labels, ind2cat)
+    test_vad(cont_preds, cont_labels)
 
 ''' Prepare test data and test models on the same'''
 def test_emotic(result_path, model_path, ind2cat, context_norm, body_norm, args):
@@ -81,4 +100,5 @@ def test_emotic(result_path, model_path, ind2cat, context_norm, body_norm, args)
     print ('test loader ', len(test_loader))
     
     device = torch.device("cuda:%s" %(str(args.gpu)) if torch.cuda.is_available() else "cpu")
-    test_data([model_context, model_body, emotic_model], device, test_loader, ind2cat, test_dataset.__len__(), save_results=True, result_dir=result_path)
+    test_data([model_context, model_body, emotic_model], device, test_loader, ind2cat, test_dataset.__len__(), result_dir=result_path, test_type='test')
+
