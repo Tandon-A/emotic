df2c2e7bdcc2866948611e0f8b4308cfd6392649
Abhishek Tandon <abhishektnd@gmail.com>
2020-04-26 20:07:11 +0200

    Updated yolo_inference.py
    Added support to get bounding box from yolo and then do inference using emotic model

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,44 +1,114 @@
 import numpy as np 
 import os 
 import cv2
+import argparse 
 
 import torch 
 from torchvision import transforms
 
-from yolo_utils import prepare_yolo, nms_people, rescale_boxes
+from inference import infer
+from yolo_utils import prepare_yolo, rescale_boxes, non_max_suppression
 
-def get_bbox(yolo_model, device, image_context_path, bbox = None, conf_thresh=0.8, nms_thresh=0.4):
-  image_context =  cv2.cvtColor(cv2.imread(image_context_path), cv2.COLOR_BGR2RGB)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', type=int, default=0, help='gpu id')
+    parser.add_argument('--experiment_path', type=str, required=True, help='Path of experiment files (results, models, logs)')
+    parser.add_argument('--model_dir', type=str, default='models', help='Folder to access the models')
+    parser.add_argument('--result_dir', type=str, default='results', help='Path to save the results')
+    parser.add_argument('--inference_file', type=str, help='Text file containing image context paths and bounding box')
+    # Generate args
+    args = parser.parse_args()
+    return args
+
+
+def get_bbox(yolo_model, device, image_context, yolo_image_size=416, conf_thresh=0.8, nms_thresh=0.4):
   test_transform = transforms.Compose([transforms.ToPILImage(),transforms.ToTensor()])
   image_yolo = test_transform(cv2.resize(image_context, (416, 416))).unsqueeze(0).to(device)
 
   with torch.no_grad():
     detections = yolo_model(image_yolo)
-    print (detections.shape)
-    detections = nms_people(detections, conf_thresh, nms_thresh)
-    print (len(detections))
+    nms_det  = non_max_suppression(detections, conf_thresh, nms_thresh)[0]
+    det = rescale_boxes(nms_det, yolo_image_size, (image_context.shape[:2]))
+  
+  bboxes = []
+  for x1, y1, x2, y2, _, _, cls_pred in det:
+    if cls_pred == 0:
+      x1 = int(x1)
+      x2 = int(x2)
+      y1 = int(y1)
+      y2 = int(y2)
+      bboxes.append([x1, y1, x2, y2])
+  return np.array(bboxes)
 
-  image = cv2.rectangle(image_context, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 3) 
-  cv2.imwrite('/home/abtandon/proj/data/debug_exp/img.jpg', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-
-  return [0, 0, 0, 0]
-
-def yolo_infer(model_dir, images_list):
-  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-  yolo = prepare_yolo(model_dir)
+def yolo_infer(images_list, result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args):
+  device = torch.device("cuda:%s" %(str(args.gpu)) if torch.cuda.is_available() else "cpu")
+  yolo = prepare_yolo(model_path)
   yolo = yolo.to(device)
   yolo.eval()
+
+  thresholds = torch.FloatTensor(np.load(os.path.join(result_path, 'val_thresholds.npy'))).to(device) 
+  model_context = torch.load(os.path.join(model_path,'model_context1.pth')).to(device)
+  model_body = torch.load(os.path.join(model_path,'model_body1.pth')).to(device)
+  emotic_model = torch.load(os.path.join(model_path,'model_emotic1.pth')).to(device)
+  models = [model_context, model_body, emotic_model]
 
   with open(images_list, 'r') as f:
     lines = f.readlines()
   
   for idx, line in enumerate(lines):
     image_context_path, x1, y1, x2, y2 = line.split('\n')[0].split(' ')
+    image_context = cv2.cvtColor(cv2.imread(image_context_path), cv2.COLOR_BGR2RGB)
     bbox = [int(x1), int(y1), int(x2), int(y2)]
-    bbox2 = get_bbox(yolo, device, image_context_path, bbox)
-    print ('pred ', bbox2, 'gt', bbox)
-    
+    bbox_yolo = get_bbox(yolo, device, image_context)
+    for pred_bbox in bbox_yolo:
+      pred_cat, pred_cont = infer(context_norm, body_norm, ind2cat, ind2vad, device, thresholds, models, image_context_path=image_context_path, bbox=pred_bbox)
+      write_text_vad = list()
+      for continuous in pred_cont:
+        write_text_vad.append(str('%.1f' %(continuous)))
+      write_text_vad = 'vad ' + ' '.join(write_text_vad) 
+      image_context = cv2.rectangle(image_context, (pred_bbox[0], pred_bbox[1]),(pred_bbox[2] , pred_bbox[3]), (255, 0, 0), 3)
+      cv2.putText(image_context, write_text_vad, (pred_bbox[0], pred_bbox[1] - 5), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+      for i, emotion in enumerate(pred_cat):
+        cv2.putText(image_context, emotion, (pred_bbox[0], pred_bbox[1] + (i+1)*12), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+    image_context = cv2.rectangle(image_context, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 3) 
+    cv2.imwrite('/home/abtandon/proj/data/debug_exp/img_%r.jpg' %(idx), cv2.cvtColor(image_context, cv2.COLOR_RGB2BGR))
+    print ('completed inference for image %d'  %(idx))
 
+def check_paths(args):
+  if not os.path.exists(args.inference_file):
+    raise ValueError('inference file does not exist. Please pass a valid inference file')
+  model_path = os.path.join(args.experiment_path, args.model_dir)
+  if not os.path.exists(model_path):
+    raise ValueError('model path %s does not exist. Please pass a valid model_path' %(model_path))
+  result_path = os.path.join(args.experiment_path, args.result_dir)
+  if not os.path.exists(result_path):
+    os.makedirs(result_path)
+  return result_path, model_path
 
+if __name__=='__main__':
+  args = parse_args()
 
-yolo_infer('/home/abtandon/proj/data/debug_exp/models', '/home/abtandon/proj/data/debug_exp/inference_list.txt')
+  result_path, model_path = check_paths(args)
+
+  cat = ['Affection', 'Anger', 'Annoyance', 'Anticipation', 'Aversion', 'Confidence', 'Disapproval', 'Disconnection', \
+          'Disquietment', 'Doubt/Confusion', 'Embarrassment', 'Engagement', 'Esteem', 'Excitement', 'Fatigue', 'Fear','Happiness', \
+          'Pain', 'Peace', 'Pleasure', 'Sadness', 'Sensitivity', 'Suffering', 'Surprise', 'Sympathy', 'Yearning']
+  cat2ind = {}
+  ind2cat = {}
+  for idx, emotion in enumerate(cat):
+      cat2ind[emotion] = idx
+      ind2cat[idx] = emotion
+  
+  vad = ['Valence', 'Arousal', 'Dominance']
+  ind2vad = {}
+  for idx, continuous in enumerate(vad):
+      ind2vad[idx] = continuous
+  
+  context_mean = [0.4690646, 0.4407227, 0.40508908]
+  context_std = [0.2514227, 0.24312855, 0.24266963]
+  body_mean = [0.43832874, 0.3964344, 0.3706214]
+  body_std = [0.24784276, 0.23621225, 0.2323653]
+  context_norm = [context_mean, context_std]
+  body_norm = [body_mean, body_std]
+
+  yolo_infer(args.inference_file, result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args)
