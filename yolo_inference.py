7f1eb24c9b5890fd16c4b5ad11a1bc505799aad3
Abhishek Tandon <abhishektnd@gmail.com>
2020-04-27 21:32:06 +0200

    Updated yolo_inference.py
    Added support for performing inference over a video

--- yolo_inference.py
+++ yolo_inference.py
@@ -16,6 +16,7 @@ def parse_args():
     parser.add_argument('--model_dir', type=str, default='models', help='Folder to access the models')
     parser.add_argument('--result_dir', type=str, default='results', help='Path to save the results')
     parser.add_argument('--inference_file', type=str, help='Text file containing image context paths and bounding box')
+    parser.add_argument('--video_file', type=str, help='Test video file')
     # Generate args
     args = parser.parse_args()
     return args
@@ -33,10 +34,10 @@ def get_bbox(yolo_model, device, image_context, yolo_image_size=416, conf_thresh
   bboxes = []
   for x1, y1, x2, y2, _, _, cls_pred in det:
     if cls_pred == 0:
-      x1 = int(x1)
-      x2 = int(x2)
-      y1 = int(y1)
-      y2 = int(y2)
+      x1 = int(min(image_context.shape[1], max(0, x1)))
+      x2 = int(min(image_context.shape[1], max(x1, x2)))
+      y1 = int(min(image_context.shape[0], max(15, y1)))
+      y2 = int(min(image_context.shape[0], max(y1, y2)))
       bboxes.append([x1, y1, x2, y2])
   return np.array(bboxes)
 
@@ -61,7 +62,7 @@ def yolo_infer(images_list, result_path, model_path, context_norm, body_norm, in
     bbox = [int(x1), int(y1), int(x2), int(y2)]
     bbox_yolo = get_bbox(yolo, device, image_context)
     for pred_bbox in bbox_yolo:
-      pred_cat, pred_cont = infer(context_norm, body_norm, ind2cat, ind2vad, device, thresholds, models, image_context_path=image_context_path, bbox=pred_bbox)
+      pred_cat, pred_cont = infer(context_norm, body_norm, ind2cat, ind2vad, device, thresholds, models, image_context=image_context, bbox=pred_bbox, to_print=False)
       write_text_vad = list()
       for continuous in pred_cont:
         write_text_vad.append(str('%.1f' %(continuous)))
@@ -71,12 +72,62 @@ def yolo_infer(images_list, result_path, model_path, context_norm, body_norm, in
       for i, emotion in enumerate(pred_cat):
         cv2.putText(image_context, emotion, (pred_bbox[0], pred_bbox[1] + (i+1)*12), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
     image_context = cv2.rectangle(image_context, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 3) 
-    cv2.imwrite('/home/abtandon/proj/data/debug_exp/img_%r.jpg' %(idx), cv2.cvtColor(image_context, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(os.path.join(result_path, 'img_%r.jpg' %(idx)), cv2.cvtColor(image_context, cv2.COLOR_RGB2BGR))
     print ('completed inference for image %d'  %(idx))
 
+def yolo_video(video_file, result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args):
+  device = torch.device("cuda:%s" %(str(args.gpu)) if torch.cuda.is_available() else "cpu")
+  yolo = prepare_yolo(model_path)
+  yolo = yolo.to(device)
+  yolo.eval()
+
+  thresholds = torch.FloatTensor(np.load(os.path.join(result_path, 'val_thresholds.npy'))).to(device) 
+  model_context = torch.load(os.path.join(model_path,'model_context1.pth')).to(device)
+  model_body = torch.load(os.path.join(model_path,'model_body1.pth')).to(device)
+  emotic_model = torch.load(os.path.join(model_path,'model_emotic1.pth')).to(device)
+  model_context.eval()
+  model_body.eval()
+  emotic_model.eval()
+  models = [model_context, model_body, emotic_model]
+
+  video_stream = cv2.VideoCapture(video_file)
+  writer = None
+
+  print ('Starting testing')
+  while True:
+    (grabbed, frame) = video_stream.read()
+    if not grabbed:
+      break
+    image_context = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    bbox_yolo = get_bbox(yolo, device, image_context)
+    for pred_bbox in bbox_yolo:
+      pred_cat, pred_cont = infer(context_norm, body_norm, ind2cat, ind2vad, device, thresholds, models, image_context=image_context, bbox=pred_bbox, to_print=False)
+      write_text_vad = list()
+      for continuous in pred_cont:
+        write_text_vad.append(str('%.1f' %(continuous)))
+      write_text_vad = 'vad ' + ' '.join(write_text_vad) 
+      image_context = cv2.rectangle(image_context, (pred_bbox[0], pred_bbox[1]),(pred_bbox[2] , pred_bbox[3]), (255, 0, 0), 3)
+      cv2.putText(image_context, write_text_vad, (pred_bbox[0], pred_bbox[1] - 5), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+      # for i, emotion in enumerate(pred_cat):
+      #   cv2.putText(image_context, emotion, (pred_bbox[0], pred_bbox[1] + (i+1)*12), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+    
+    if writer is None:
+      fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+      writer = cv2.VideoWriter(os.path.join(result_path, 'result_vid.avi'), fourcc, 30, (image_context.shape[1], image_context.shape[0]), True)  
+    writer.write(cv2.cvtColor(image_context, cv2.COLOR_RGB2BGR))
+  writer.release()
+  video_stream.release() 
+  print ('Completed video')
+
+
 def check_paths(args):
-  if not os.path.exists(args.inference_file):
-    raise ValueError('inference file does not exist. Please pass a valid inference file')
+  if args.inference_file is not None: 
+    if not os.path.exists(args.inference_file):
+      raise ValueError('inference file does not exist. Please pass a valid inference file')
+  if args.video_file is not None: 
+    if not os.path.exists(args.video_file):
+      raise ValueError('video file does not exist. Please pass a valid video file')
   model_path = os.path.join(args.experiment_path, args.model_dir)
   if not os.path.exists(model_path):
     raise ValueError('model path %s does not exist. Please pass a valid model_path' %(model_path))
@@ -111,4 +162,9 @@ if __name__=='__main__':
   context_norm = [context_mean, context_std]
   body_norm = [body_mean, body_std]
 
-  yolo_infer(args.inference_file, result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args)
+  if args.inference_file is not None: 
+    print ('inference over inference file images')
+    yolo_infer(args.inference_file, result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args)
+  if args.video_file is not None:
+    print ('inference over test video')
+    yolo_video(args.video_file, result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args)
